ec01ae850f0b69f4c757974408d51df8a49f5544
shaoningk <ji_mama@hotmail.com>
2014-09-12 17:23:58 +0800

    Fixed skeleton file
    Code contained wrong syntax

--- dora/mds_adapter.py
+++ dora/mds_adapter.py
@@ -1,16 +1,7 @@
-import urllib2, sys, psycopg2, settings
-
+import sys, urllib2
+from .models import Patient, Disease
 
 def main(argv):
-    
-    database = setting.default
-    try:
-        setting_parameters = "dbname='%s' user='%s' host='%s' password='%s'" % (database.NAME, database.USER, 
-                                                                                database.HOST, database.PASSWORD)
-        conn = psycopg2.connect(setting_parameters)
-    except:
-        print "Error: Cannot connect to the database."
-        sys.exit(0)
     
     url = r'http://loaskjwhvfwkjhev'
     response = urllib2.urlopen(url).read()
@@ -18,7 +9,7 @@ def main(argv):
     if response.status == 'SUCCESS':
         subjects = get_subjects(response.message)
         diseases = get_diseases(response.message)
-        populate_database(subjects, diseases, con)
+        populate_database(subjects, diseases)
     
     return 0
     
@@ -31,5 +22,5 @@ def get_subjects(list):
 def get_diseases(list):
     pass
 
-def populate_database(subjects, diseases, db):
+def populate_database(subjects, diseases):
     pass
