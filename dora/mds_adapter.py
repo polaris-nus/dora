807433ea588df545f624c45df612e1e61782fbe1
jyzackoh <zackkohjunyang@gmail.com>
2014-09-13 00:07:12 +0800

    Implemented in mds_adapter.py get_gps_list_and_diagnosis()
    Implemented the get_gps_list_and_diagnosis() to get data from mds
    server. Also fixed some syntax errors in mds_adapter.py

--- dora/mds_adapter.py
+++ dora/mds_adapter.py
@@ -1,26 +1,46 @@
-import sys, urllib2
-from .models import Patient, Disease
+import sys, urllib2, json
+#from dora.dora.models import Patient, Disease
 
-def main(argv):
-    
-    url = r'http://loaskjwhvfwkjhev'
-    response = urllib2.urlopen(url).read()
 
-    if response.status == 'SUCCESS':
-        subjects = get_subjects(response.message)
-        diseases = get_diseases(response.message)
-        populate_database(subjects, diseases)
-    
-    return 0
-    
-if __name__ == "__main__":
-    main(sys.argv)
-    
-def get_subjects(list):
-    pass
+def get_diagnosis_and_gps_lists(list):
+	
+	observation_list = (json.loads(list))['message']
+	diagnosis_list = []
+	gps_list = []
 
-def get_diseases(list):
-    pass
+	for observation in observation_list:
+		if observation['concept']['name'] == "LOCATION GPS":
+			gps_list.append(observation)
+		elif observation['concept']['name'] == "DIAGNOSIS":
+			diagnosis_list.append(observation)
+
+	return diagnosis_list, gps_list
+
 
 def populate_database(subjects, diseases):
-    pass
+	pass
+
+
+def main(argv):
+
+	url = "http://mdstest.codeofjoy.com/core/observation/"
+	response = urllib2.urlopen(url)
+
+	if response.getcode() == 200:
+		diagnosis_list = []
+		gps_list = []
+
+		diagnosis_list,gps_list = get_diagnosis_and_gps_lists(response.read())
+
+		#This is for debugging/checking purposes
+		for diagnosis in diagnosis_list:
+			print diagnosis['concept']['name'] + " " + diagnosis['encounter']['created']
+		for gps in gps_list:
+			print gps['concept']['name'] + " " + gps['encounter']['created']
+		#end of debugging/checking block
+
+		populate_database(diagnosis_list,gps_list)
+	return 0
+
+if __name__ == "__main__":
+	main(sys.argv)
