1e7356fe021d7117def80c2e5f2d4535e0b596d9
shaoningk <ji_mama@hotmail.com>
2014-09-15 00:53:34 +0800

    Renamed the mds_adapter_model.py for consistency

--- dora/mds_adapter_models.py
+++ dora/mds_adapter_models.py
@@ -1,18 +1,17 @@
 from django.contrib.gis.db import models
+from datetime import date
 
 class LastSynchronised(models.Model):
 
-    last_synchronised = models.DateTimeField()
+    last_synchronised = models.DateTimeField(default=date.min)
     
     def __unicode__():
         return last_synchronised
     
 class PatientLookupTable(models.Model):
-    
     uuid = models.CharField(max_length=128, primary_key=True)
     subject = models.ForeignKey('Patient')
     
 class DiseaseLookupTable(models.Model):
-    
     uuid = models.CharField(max_length=128, primary_key=True)
     disease = models.ForeignKey('Disease')
