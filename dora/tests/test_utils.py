39349e53ea380f177fe12c9d11830593e4f72a38
jyzackoh <zackkohjunyang@gmail.com>
2014-09-21 23:26:51 +0800

    Cleaned up test_utils.py using setUp()
    However, still decided to use util_test_helper as it makes test_utils
    much more readable.

--- dora/tests/test_utils.py
+++ dora/tests/test_utils.py
@@ -5,14 +5,17 @@ from dora.tests.util_test_helper import *
 
 
 class UtilsTestCase(TestCase):
+	def setUp(self):
+		setup_database()
+		self.factory = RequestFactory()
+
+
 	#test the query method in views
 	def test_get_QRS(self):
 		#preparation steps for test case
-		setup_database()
-		results = setup_results()
+		results = setup_get_QRS_results()
 
 		#assert one with only disease
-		self.factory = RequestFactory()
 		request = self.factory.get('/query/', {'disease': 'tubercolosis'})
 		query_result_set = utils.get_query_result_set(request)
 		check_assertions(self, query_result_set, results, 0)
@@ -26,3 +29,29 @@ class UtilsTestCase(TestCase):
 		request = self.factory.get('/query/')
 		query_result = utils.get_query_result_set(request)
 		self.assertEqual(query_result,None)
+
+	def test_create_json_obj_list(self):
+		#setup
+		results = setup_create_json_obj_list_results()
+		request = self.factory.get('/query/', {'disease': 'tubercolosis', 'gender':'M'})
+		query_result_set = utils.get_query_result_set(request)
+
+		#assert when passed in None
+		json_obj_list = utils.create_json_obj_list(None)
+		self.assertEqual(json_obj_list, [])
+
+		#assert when passed in some QRS
+		json_obj_list = utils.create_json_obj_list(query_result_set)
+		self.assertEqual(json_obj_list, results)
+
+	def test_json_obj_to_return(self):
+		#takes in list of json and returns json array
+		json_list = []
+		json_list.append("{'name':'json item 1'}")
+		json_list.append("{'name':'json item 2'}")
+		json_list.append("{'name':'json item 3'}")
+
+		results = "[\n{'name':'json item 1'},\n{'name':'json item 2'},\n{'name':'json item 3'}\n]"
+		json_array = utils.generate_json_obj_to_return(json_list)
+
+		self.assertEquals(json_array, results)
--- dora/tests/util_test_helper.py
+++ dora/tests/util_test_helper.py
@@ -1,6 +1,8 @@
 import random
 from dora.models import *
 from datetime import *
+from django.contrib.gis.geos import Point
+from django.template import Context, loader
 
 
 def setup_database():
@@ -18,10 +20,9 @@ def setup_database():
 			uuid=str(i), 
 			given_name=('abnn'+str(i)), 
 			family_name=str(i), 
-			dob=(datetime.now() - timedelta(days=(i*365*5))), 
+			dob=(date(2010, 10, 10) - timedelta(days=(i*365*5))), 
 			gender=gender,
-			coordinates=Point(random.uniform(-179.99, 180),random.uniform(-89.99, 90)), 
-			created=datetime.now()
+			coordinates=Point(-180+((i+1)/6*360),-90+((i+1)/6*180))
 		)
 		patients.append(temp_patient)
 	for i in range(0,6):
@@ -31,11 +32,11 @@ def setup_database():
 			uuid=str(i), 
 			patient=patients[i],
 			disease=disease,
-			created=datetime.now(),
-			modified=datetime.now()
+			created=date(2010, 10, 10),
+			modified=date(2010, 10, 10)
 		)
 
-def setup_results():
+def setup_get_QRS_results():
 	results = []
 	results.append([])
 	results.append([])
@@ -67,3 +68,35 @@ def check_assertions(self, query_result_set, results, check_num):
 		self.assertEqual(query_result.disease.name, results[check_num][index]['disease_name'])
 		self.assertEqual(query_result.patient.uuid, results[check_num][index]['patient_uuid'])
 		index += 1
+
+
+def setup_create_json_obj_list_results():
+	results = []
+	json_template = loader.get_template('query_json_template')
+	results.append(json_template.render(Context({
+		"disease_name":'TUBERCOLOSIS', 
+		"patient_uuid":'0', 
+		"patient_family_name":'0', 
+		"patient_given_name":'abnn0', 
+		"patient_dob":'Oct. 10, 2010, midnight',
+		"patient_gender":'M', 
+		"created_date":'Oct. 10, 2010, midnight', 
+		"modified_date":'Oct. 10, 2010, midnight', 
+		"longitude":str(-180.0+(1/6*360.0)), 
+		"latitude":str(-90.0+(1/6*180.0)), 
+		"altitude":"alt!"
+	})))
+	results.append(json_template.render(Context({
+		"disease_name":'TUBERCOLOSIS', 
+		"patient_uuid":'2', 
+		"patient_family_name":'2', 
+		"patient_given_name":'abnn2', 
+		"patient_dob":'Oct. 12, 2000, midnight',
+		"patient_gender":'M', 
+		"created_date":'Oct. 10, 2010, midnight', 
+		"modified_date":'Oct. 10, 2010, midnight', 
+		"longitude":str(-180.0+(3/6*360.0)), 
+		"latitude":str(-90.0+(3/6*180.0)), 
+		"altitude":"alt!"
+	})))
+	return results
