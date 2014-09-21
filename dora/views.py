ae1baded4fd371b879cb2cfc2bbcc1f6a401f57f
jyzackoh <zackkohjunyang@gmail.com>
2014-09-21 20:14:48 +0800

    Fixed a bug where views.py imports dora.logic
    dora.logic has already been renamed to dora.utils Updated views.py to
    fix the bug.

--- dora/views.py
+++ dora/views.py
@@ -10,7 +10,9 @@ def query(request):
 	query_result_set = get_query_result_set(request)
 
 	#Create a list of json objects
-	json_obj_list = create_json_obj_list(query_result_set)
+	json_obj_list = []
+	if (query_result_set)
+		json_obj_list = create_json_obj_list(query_result_set)
 
 	#transform json objects into one json array of objects
 	json_query_results = generate_json_obj_to_return(json_obj_list)
