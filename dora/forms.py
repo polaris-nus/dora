6679194a68db1f4103d92c199ba7aecb0cb6b677
shaoningk <ji_mama@hotmail.com>
2014-09-28 01:59:42 +0800

    Added validation for age_range in form

--- dora/forms.py
+++ dora/forms.py
@@ -1,8 +1,16 @@
 import django.contrib.gis.forms as forms
+from django.core.validators import RegexValidator
 
 class QueryForm(forms.Form):
     disease = forms.CharField(max_length=128)
     gender = forms.CharField(required=False, max_length=2,
                              widget=forms.Select(choices=(("M","M"),("F","F"))))
-    age_range = forms.CharField(required=False, max_length=128)
+    age_range = forms.CharField(required=False, max_length=128, 
+            validators=[RegexValidator(regex=r"^\s*([0-9]+\s*-\s*[0-9]+)(\s*,\s*[0-9]+\s*-\s*[0-9]+)*$")])
     location = forms.GeometryField(required=False, srid=4326)
+    
+    def clean_age_range(self):
+        age_range = self.cleaned_data['age_range']
+        age_range = age_range.replace(" ","")
+        
+        return age_range
