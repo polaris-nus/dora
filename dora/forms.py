cbd80e2fbf7736e327a1b6b97580806f867268db
shaoningk <ji_mama@hotmail.com>
2014-09-27 19:40:59 +0800

    Fixed syntax errors in forms.py

--- dora/forms.py
+++ dora/forms.py
@@ -2,6 +2,7 @@ import django.contrib.gis.forms as forms
 
 class QueryForm(forms.Form):
     disease = forms.CharField(max_length=128)
-    gender = forms.CharField(null=True, choices=(("M","M"),("F","F")), max_length=2)
-    age_range = forms.CharField(null=True, max_length=128)
-    location = forms.GeometryField(null=True, srid=4326)
+    gender = forms.CharField(required=False, max_length=2,
+                             widget=forms.Select(choices=(("M","M"),("F","F"))))
+    age_range = forms.CharField(required=False, max_length=128)
+    location = forms.GeometryField(required=False, srid=4326)
