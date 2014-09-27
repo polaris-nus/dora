22aa9f3d4b4b6c1437413783112ccea237c3c980
shaoningk <ji_mama@hotmail.com>
2014-09-27 18:42:59 +0800

    Used forms to check validity of queries in utils

--- dora/utils.py
+++ dora/utils.py
@@ -1,6 +1,7 @@
 import json, operator
 from django.shortcuts import render
 from dora.models import *
+from dora.forms import QueryForm
 from django.db.models import Q
 from datetime import *
 from django.template import Context, loader
@@ -8,35 +9,43 @@ from django.template import Context, loader
 #Returns the query result set from a given request
 #Pre: must have disease request
 def parse_request(request):
-	disease_name = request.GET.get('disease')
-	if (not disease_name):
+	query_form = QueryForm(request.GET)
+	
+	if query_form.is_valid():
+		cleaned_data = query_form.cleaned_data
+		
+		disease_name = cleaned_data.disease
+		if (not disease_name):
+			return None
+			
+		patient_gender_filter = cleaned_data.gender
+		location_filter = cleaned_data.location
+		age_range_filter = cleaned_data.age_range
+	
+		#Construct filter arguments
+		q_object = Q(disease__name__iexact=disease_name)
+	
+		if (patient_gender_filter):
+			q_object &= Q(patient__gender=patient_gender_filter)
+			
+		if (location_filter):
+			q_object &= Q(coordinates__within=location_filter)
+	
+		if (age_range_filter):
+			q_object_age_range = Q()
+			now = datetime.now()
+			age_range_filter_list = age_range_filter.split(',')
+			for age_range in age_range_filter_list:
+				age_range_list = age_range.split('-')
+				age_start = int(age_range_list[0])
+				age_end = int(age_range_list[1]) + 1
+				q_object_age_range |= (Q(patient__dob__lte=datetime(year=now.year-age_start, month=now.month, day=now.day)) & Q(patient__dob__gte=datetime(year=now.year-age_end, month=now.month, day=now.day)))
+			q_object &= q_object_age_range
+	
+		return q_object
+	
+	else:
 		return None
-		
-	patient_gender_filter = request.GET.get('gender')
-	location_filter = request.GET.get('location')
-	age_range_filter = request.GET.get('age_range')
-
-	#Construct filter arguments
-	q_object = Q(disease__name__iexact=disease_name)
-
-	if (patient_gender_filter):
-		q_object &= Q(patient__gender=patient_gender_filter)
-		
-	if (location_filter):
-		q_object &= Q(coordinates__within=location_filter)
-
-	if (age_range_filter):
-		q_object_age_range = Q()
-		now = datetime.now()
-		age_range_filter_list = age_range_filter.split(',')
-		for age_range in age_range_filter_list:
-			age_range_list = age_range.split('-')
-			age_start = int(age_range_list[0])
-			age_end = int(age_range_list[1]) + 1
-			q_object_age_range |= (Q(patient__dob__lte=datetime(year=now.year-age_start, month=now.month, day=now.day)) & Q(patient__dob__gte=datetime(year=now.year-age_end, month=now.month, day=now.day)))
-		q_object &= q_object_age_range
-
-	return q_object
 
 def get_query_result_set(query):
 	if (query == None):
