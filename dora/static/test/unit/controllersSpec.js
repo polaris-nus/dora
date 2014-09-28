89367fa47652cf93ac052d5d7e26d4bbb7468ac9
jwwong <jwwong.91@gmail.com>
2014-09-28 14:56:44 +0800

    Added skeletal test code

---
+++ dora/static/test/unit/controllersSpec.js
@@ -0,0 +1,16 @@
+describe('Dora controllers', function() {
+
+	it('is a test spec', function() {
+		expect(true).toEqual(true);
+	});
+
+	describe('QueryFormController', function() {
+
+	});
+
+	describe('QueryResultController', function() {
+		
+	});
+
+});
+
