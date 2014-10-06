fd4aedb4ab6e55d61c5a2c2dbca688178198f0af
jyzackoh <zackkohjunyang@gmail.com>
2014-10-06 12:21:33 +0800

    added test case for controller.js and fixed some bugs and cleaned the code for controller.js

--- dora/static/js/controllers.js
+++ dora/static/js/controllers.js
@@ -57,6 +57,7 @@ doraControllers.controller('QueryResultController', ['$scope', 'QRSServ', 'MapSe
 		$scope.selectedQRSList = [];
 		$scope.selectionFlag = false;
 		$scope.selectionFunction = '';
+		$scope.newQRS = {};
 
 		$scope.setDisplayedQRS = function(index) {
 			$scope.displayedQRS = $scope.QRSHistory[index];
@@ -67,12 +68,21 @@ doraControllers.controller('QueryResultController', ['$scope', 'QRSServ', 'MapSe
 			MapServ.toggleClusterLayerVisibility($scope.doubleClickedQRS);
 		}
 
+		//--Start Union intersection Methods--//
+		$scope.resetUnionIntersectVariables = function(){
+			$scope.selectedQRSList = [];
+			$scope.selectionFlag = false;
+			$scope.selectionFunction = '';
+		}
+
 		$scope.selectQRS = function(QRS){
 			var indexOfQRS = $scope.selectedQRSList.indexOf(QRS);
-			if ($scope.selectionFlag && indexOfQRS === -1) {
-				$scope.selectedQRSList.push(QRS);
-			} else {
-				$scope.selectedQRSList.splice(indexOfQRS, 1);
+			if ($scope.selectionFlag) {
+				if (indexOfQRS === -1) {
+					$scope.selectedQRSList.push(QRS);
+				} else {
+					$scope.selectedQRSList.splice(indexOfQRS, 1);
+				}
 			}
 
 			console.log(QRS);
@@ -81,24 +91,20 @@ doraControllers.controller('QueryResultController', ['$scope', 'QRSServ', 'MapSe
 
 		$scope.unionIntersectQRS = {
 			union: function(){
-				var newQRS = QRSServ.unionQRSHistory($scope.selectedQRSList);
-				console.log("BEIOWN");
+				$scope.newQRS = QRSServ.unionQRSHistory($scope.selectedQRSList);
+				console.log("union called");
 			},
 			intersect: function(){
-				var newQRS = QRSServ.intersectQRSHistory($scope.selectedQRSList);
-				console.log("BEIOWN ALSO");
+				$scope.newQRS = QRSServ.intersectQRSHistory($scope.selectedQRSList);
+				console.log("intersect called");
 			}
 		};
 
 		$scope.executeUnionIntersection = function(){
 			console.log($scope.selectionFunction);
 			$scope.unionIntersectQRS[$scope.selectionFunction]();
-
-			//reset
-			$scope.selectedQRSList = [];
-			$scope.selectionFlag = false;
-			$scope.selectionFunction = '';
+			$scope.resetUnionIntersectVariables();
 		};
-
+		//--End Union intersection Methods--// 
 	}
 ]);
--- dora/static/test/unit/controllersSpec.js
+++ dora/static/test/unit/controllersSpec.js
@@ -31,8 +31,254 @@ describe('Dora controllers', function() {
 	});
 
 	describe('QueryResultController', function() {
-		
-	});
+    var QRSServ;
+
+    beforeEach(inject(function($rootScope, $controller, _QRSServ_) {
+      scope = $rootScope.$new();
+      ctrl = $controller('QueryResultController', {$scope: scope});
+      QRSServ = _QRSServ_;
+      QRS1 = {
+        'assigned':[
+          {
+            "disease": "EBOLA",
+            "patient": {
+              "uuid": "one"
+            },
+            "location": {
+              "coords": "POINT (-87.1134192674000050 -4.0753749522299998)",
+              "alt": "alt!"
+            }
+          }
+        ],
+        'unassigned':[
+          {
+            "disease": "EBOLA",
+            "patient": {
+              "uuid": "three"
+            }
+          }
+        ]
+      };
+
+      QRS2 = {
+        'assigned':[
+          {
+            "disease": "TB",
+            "patient": {
+              "uuid": "four"
+            },
+            "location": {
+              "coords": "POINT (158.4795246920000100 74.5477679921000060)",
+              "alt": "alt!"
+            }
+          }
+        ],
+        'unassigned':[
+          {
+            "disease": "TB",
+            "patient": {
+              "uuid": "three"
+            }
+          }
+        ]
+      };
+
+      QRS3 = {
+        'assigned':[
+          {
+            "disease": "HIV",
+            "patient": {
+              "uuid": "six"
+            },
+            "location": {
+              "coords": "POINT (40.3393490166000030 -41.1745275103999970)",
+              "alt": "alt!"
+            }
+          }
+        ],
+        'unassigned':[
+          {
+            "disease": "HIV",
+            "patient": {
+              "uuid": "three"
+            }
+          }
+        ]
+      };
+      QRSServ.addToQRSHistory(QRS1);
+      QRSServ.addToQRSHistory(QRS2);
+      QRSServ.addToQRSHistory(QRS3);
+
+      //Make sure variables are always initiated properly
+      expect(scope.selectedQRSList).toEqual([]);
+      expect(scope.selectionFlag).toBe(false);
+      expect(scope.selectionFunction).toBe('');
+
+    }));
+
+    it('should be able to toggle choosing of union', function() {
+      var unionAnswer = {
+        'assigned':[
+          {
+            "disease": "EBOLA",
+            "patient": {
+              "uuid": "one"
+            },
+            "location": {
+              "coords": "POINT (-87.1134192674000050 -4.0753749522299998)",
+              "alt": "alt!"
+            }
+          },
+          {
+            "disease": "TB",
+            "patient": {
+              "uuid": "four"
+            },
+            "location": {
+              "coords": "POINT (158.4795246920000100 74.5477679921000060)",
+              "alt": "alt!"
+            }
+          },
+          {
+            "disease": "HIV",
+            "patient": {
+              "uuid": "six"
+            },
+            "location": {
+              "coords": "POINT (40.3393490166000030 -41.1745275103999970)",
+              "alt": "alt!"
+            }
+          }
+        ],
+        'unassigned':[
+          {
+            "disease": "EBOLA",
+            "patient": {
+              "uuid": "three"
+            }
+          },
+          {
+            "disease": "TB",
+            "patient": {
+              "uuid": "three"
+            }
+          },
+          {
+            "disease": "HIV",
+            "patient": {
+              "uuid": "three"
+            }
+          }
+        ]
+      };
+      //Allow for selection of QRS
+      scope.selectionFlag = true;
+      scope.selectionFunction = 'union';
+
+      scope.selectQRS(0);
+      scope.selectQRS(1);
+      scope.selectQRS(2);
+      expect(scope.selectedQRSList).toEqual([0,1,2]);
+
+      scope.selectQRS(2);
+      expect(scope.selectedQRSList).toEqual([0,1]);
+
+      scope.selectQRS(2);
+      expect(scope.selectedQRSList).toEqual([0,1,2]);
+
+      //Make sure variables are always reset
+      scope.executeUnionIntersection();
+      var QRSHistory = QRSServ.getQRSHistory();
+
+      //Check individula arrays by iterating
+      assignedList = QRSHistory[3].assigned;
+      unassignedList = QRSHistory[3].unassigned;
+      assignedAnswer = unionAnswer.assigned;
+      unassignedAnswer = unionAnswer.unassigned;
+
+      //Start Testing - Need to test the two lists separately due to the extra ID field added in.
+      expect(assignedList.length).toBe(assignedAnswer.length);
+      for (var i = 0; i < assignedList.length; i++) {
+        expect(assignedList[i]).toEqual(assignedAnswer[i]);
+      }
+
+      expect(unassignedList.length).toBe(unassignedAnswer.length);
+      for (var i = 0; i < unassignedList.length; i++) {
+        expect(unassignedList[i]).toEqual(unassignedAnswer[i]);
+      }
+
+      expect(scope.selectedQRSList).toEqual([]);
+      expect(scope.selectionFlag).toBe(false);
+      expect(scope.selectionFunction).toBe('');
+    });    
+
+    it('should be able to toggle choosing of intersect', function() {
+      var intersectAnswer = {
+        'assigned':[],
+        'unassigned':[
+          {
+            "disease": "EBOLA",
+            "patient": {
+              "uuid": "three"
+            }
+          },
+          {
+            "disease": "TB",
+            "patient": {
+              "uuid": "three"
+            }
+          },
+          {
+            "disease": "HIV",
+            "patient": {
+              "uuid": "three"
+            }
+          }
+        ]
+      };
+      //Allow for selection of QRS
+      scope.selectionFlag = true;
+      scope.selectionFunction = 'intersect';
+
+      scope.selectQRS(0);
+      scope.selectQRS(1);
+      scope.selectQRS(2);
+      expect(scope.selectedQRSList).toEqual([0,1,2]);
+
+      scope.selectQRS(2);
+      expect(scope.selectedQRSList).toEqual([0,1]);
+
+      scope.selectQRS(2);
+      expect(scope.selectedQRSList).toEqual([0,1,2]);
+
+      //Make sure variables are always reset
+      scope.executeUnionIntersection();
+      var QRSHistory = QRSServ.getQRSHistory();
+
+
+      //Check individula arrays by iterating
+      assignedList = QRSHistory[3].assigned;
+      unassignedList = QRSHistory[3].unassigned;
+      assignedAnswer = intersectAnswer.assigned;
+      unassignedAnswer = intersectAnswer.unassigned;
+
+      //Start Testing - Need to test the two lists separately due to the extra ID field added in.
+      expect(assignedList.length).toBe(assignedAnswer.length);
+      for (var i = 0; i < assignedList.length; i++) {
+        expect(assignedList[i]).toEqual(assignedAnswer[i]);
+      }
+
+      expect(unassignedList.length).toBe(unassignedAnswer.length);
+      for (var i = 0; i < unassignedList.length; i++) {
+        expect(unassignedList[i]).toEqual(unassignedAnswer[i]);
+      }
+
+      expect(scope.selectedQRSList).toEqual([]);
+      expect(scope.selectionFlag).toBe(false);
+      expect(scope.selectionFunction).toBe('');
+    });    
+
+  });
 
 });
 
