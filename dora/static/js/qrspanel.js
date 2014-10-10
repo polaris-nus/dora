518ef457196ced89781c851db1968db34470f8a1
tangyixuan <ayixuan@hotmail.com>
2014-10-10 10:43:50 +0800

    edit control for query panel

--- dora/static/js/qrspanel.js
+++ dora/static/js/qrspanel.js
@@ -1,24 +1,3 @@
-$(document).ready(function(){
-	// hide patient list at beginning
-	$("#pl").hide();
-
-	//toggle the visibility of patient list 
-	$("#togglePL").click(function(){
-		$("#pl").toggle();
-	});
-
-	// read patient list from json file
-	$.getJSON('qrs.json', function(data) {
-		var output="<br/>";
-		for (var i in data.patients) {
-		output+="<p>" + data.patients[i].firstName + " " + data.patients[i].lastName + " - " + data.patients[i].joined.month+" "+data.patients[i].joined.day+"th, "+data.patients[i].joined.year+"</p>";
-		}
-		output+="<a href=\"#\" class=\"patient-name\" data-toggle=\"popover\" title=\"Patient Detail Information\" data-content=\"Name: Tom, Gender: Male\" role=\"button\">Tom</a>";
-	
-		document.getElementById("pl").innerHTML=output;
-	});
-});
-
 // Load the Visualization API and the piechart package.
 google.load('visualization', '1.0', {'packages':['corechart']});
 
@@ -68,3 +47,25 @@ function drawChart() {
 
   chart2.draw(data2, options2);
 }
+
+
+$(document).ready(function(){
+  // hide patient list at beginning
+  $("#pl").hide();
+
+  //toggle the visibility of patient list 
+  $("#togglePL").click(function(){
+    $("#pl").toggle();
+  });
+
+  // read patient list from json file
+  $.getJSON('qrs.json', function(data) {
+    var output="<br/>";
+    for (var i in data.patients) {
+    output+="<p>" + data.patients[i].firstName + " " + data.patients[i].lastName + " - " + data.patients[i].joined.month+" "+data.patients[i].joined.day+"th, "+data.patients[i].joined.year+"</p>";
+    }
+    output+="<a href=\"#\" class=\"patient-name\" data-toggle=\"popover\" title=\"Patient Detail Information\" data-content=\"Name: Tom, Gender: Male\" role=\"button\">Tom</a>";
+  
+    document.getElementById("pl").innerHTML=output;
+  });
+});
