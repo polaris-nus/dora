efb3fc65eaf3a5539bc8004ae7da5fc615ea431e
jwwong <jwwong.91@gmail.com>
2014-10-17 14:32:46 +0800

    Implemented dynamic assignment of colours to QRS buttons and markers

--- dora/static/js/services.js
+++ dora/static/js/services.js
@@ -269,7 +269,7 @@ doraServices.service('MapServ', [
 				var features = [];
 			  for (index in coordinates){
 			    var vectorFeature = wktParser.read(coordinates[index].coords);
-			    vectorFeature.attributes = {featureColor: QRS.color, date: coordinates[index].created};
+			    vectorFeature.attributes = {featureColor: QRS.color.markerColor, date: coordinates[index].created};
 			    features.push(vectorFeature);
 			  }
 
@@ -523,16 +523,16 @@ doraServices.service('MapServ', [
 doraServices.service('PaletteServ', [
 	function(){
 		var palette = [
-			{color:'Crimson', inUse: false},
-			{color:'GoldenRod', inUse: false},
-			{color:'Gold', inUse: false},
-			{color:'GreenYellow', inUse: false},
-			{color:'ForestGreen', inUse: false},
-			{color:'DeepSkyBlue', inUse: false},
-			{color:'DodgerBlue', inUse: false},
-			{color:'MidnightBlue', inUse: false},
-			{color:'Indigo', inUse: false},
-			{color:'MediumVioletRed', inUse: false}
+			{cssIndex: 0, color:'#D46A6A', inUse: false},
+			{cssIndex: 1, color:'#FFBB45', inUse: false},
+			{cssIndex: 2, color:'#FFDD49', inUse: false},
+			{cssIndex: 3, color:'#B0E353', inUse: false},
+			{cssIndex: 4, color:'#41B368', inUse: false},
+			{cssIndex: 5, color:'#3D7C9B', inUse: false},
+			{cssIndex: 6, color:'#4C55A9', inUse: false},
+			{cssIndex: 7, color:'#6E46A6', inUse: false},
+			{cssIndex: 8, color:'#B94395', inUse: false},
+			{cssIndex: 9, color:'#E15273', inUse: false}
 		]
 
 		return {
@@ -540,14 +540,18 @@ doraServices.service('PaletteServ', [
 				for(index in palette) {
 					if(!palette[index].inUse) {
 						palette[index].inUse = true;
-						return palette[index].color;
+						var color = {
+							markerColor: palette[index].color,
+							buttonStyleIndex: palette[index].cssIndex
+						};
+						return color;
 					}
 				}
 				return null; // all colors in use
 			},
 			releaseColor: function(color) {
 				for(index in palette) {
-					if(palette[index].color.localeCompare(color) == 0) {
+					if(palette[index].color.localeCompare(color.markerColor) == 0) {
 						palette[index].inUse = false;
 						break;
 					}
