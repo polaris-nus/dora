2231441d21dc17cf606a8b388862f4d5e4822e2d
shaoningk <ji_mama@hotmail.com>
2014-10-27 16:48:57 +0800

    Created new models for saved queries for users

--- sana.mds-master/src/mds/dora/models.py
+++ sana.mds-master/src/mds/dora/models.py
@@ -1,5 +1,6 @@
 from django.contrib.gis.db import models
 from mds.core.models import Encounter, Observation
+from django.contrib.auth.models import User
 
 class EncounterLocation(models.Model):
 	"""A mapping of encounters to the location of the encounters from its GPS location observation"""
@@ -13,3 +14,10 @@ class EncounterLocation(models.Model):
 	observation = models.ForeignKey(Observation, to_field='uuid', on_delete=models.CASCADE)
 	
 import mds.dora.signals
+
+class SavedQuery(models.Model):
+	"""A mapping of saved queries to the user who saved them"""
+	
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	
+	query = models.TextField()
